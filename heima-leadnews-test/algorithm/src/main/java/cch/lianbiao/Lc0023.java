5bdfaa5cf56af6a08f1a7e06a83a17a7e0705382
s-chchen <1843093222@qq.com>
2024-04-12 00:57:06 +0800

    链表和数组的算法

---
+++ heima-leadnews-test/algorithm/src/main/java/cch/lianbiao/Lc0023.java
@@ -0,0 +1,85 @@
+package cch.lianbiao;
+
+import java.util.PriorityQueue;
+
+/**
+ * LC0023. 合并 K 个升序链表
+ * 困难
+ * 相关标签
+ * 相关企业
+ * 给你一个链表数组，每个链表都已经按升序排列。
+ *
+ * 请你将所有链表合并到一个升序链表中，返回合并后的链表。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ * 输入：lists = [[1,4,5],[1,3,4],[2,6]]
+ * 输出：[1,1,2,3,4,4,5,6]
+ */
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class Lc0023 {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        // solution.mergeKLists();
+    }
+
+}
+
+
+class Solution {
+    class Status implements Comparable<Status> {
+        int val;
+        ListNode ptr;
+
+        Status(int val, ListNode ptr) {
+            this.val = val;
+            this.ptr = ptr;
+        }
+
+        public int compareTo(Status status2) {
+            return this.val - status2.val;
+        }
+    }
+
+    PriorityQueue<Status> queue = new PriorityQueue<Status>();
+
+    public ListNode mergeKLists(ListNode[] lists) {
+        for (ListNode node: lists) {
+            if (node != null) {
+                queue.offer(new Status(node.val, node));
+            }
+        }
+        ListNode head = new ListNode(0);
+        ListNode tail = head;
+        while (!queue.isEmpty()) {
+            Status f = queue.poll();
+            tail.next = f.ptr;
+            tail = tail.next;
+            if (f.ptr.next != null) {
+                queue.offer(new Status(f.ptr.next.val, f.ptr.next));
+            }
+        }
+        return head.next;
+    }
+} 
+
